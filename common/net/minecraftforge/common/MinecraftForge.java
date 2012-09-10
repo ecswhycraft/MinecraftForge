7fc0d3895f4d0b5e3508a0a4d08ae69b0b39de70
Christian <cpw@weeksfamily.ca>
2012-09-09 23:47:07 -0400

    Simple chunkloading implementation

--- common/net/minecraftforge/common/MinecraftForge.java
+++ common/net/minecraftforge/common/MinecraftForge.java
@@ -3,6 +3,11 @@ package net.minecraftforge.common;
 import java.lang.reflect.Constructor;
 import java.util.*;
 
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.FMLLog;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.ModContainer;
+
 import net.minecraft.src.*;
 import net.minecraftforge.common.ForgeHooks.GrassEntry;
 import net.minecraftforge.common.ForgeHooks.SeedEntry;
@@ -13,14 +18,14 @@ import net.minecraftforge.event.entity.EntityEvent;
 public class MinecraftForge
 {
     /**
-     * The core Forge EventBus, all events for Forge will be fired on this, 
+     * The core Forge EventBus, all events for Forge will be fired on this,
      * you should use this to register all your listeners.
      * This replaces every register*Handler() function in the old version of Forge.
      */
     public static final EventBus EVENT_BUS = new EventBus();
     public static boolean SPAWNER_ALLOW_ON_INVERTED = false;
-    
-    
+
+
     /** Register a new plant to be planted when bonemeal is used on grass.
      * @param block The block to place.
      * @param metadata The metadata to set for the block when being placed.
@@ -32,25 +37,25 @@ public class MinecraftForge
         ForgeHooks.grassList.add(new GrassEntry(block, metadata, weight));
     }
 
-    /** 
+    /**
      * Register a new seed to be dropped when breaking tall grass.
-     * 
+     *
      * @param seed The item to drop as a seed.
-     * @param weight The relative probability of the seeds, 
+     * @param weight The relative probability of the seeds,
      *               where wheat seeds are 10.
      */
     public static void addGrassSeed(ItemStack seed, int weight)
     {
         ForgeHooks.seedList.add(new SeedEntry(seed, weight));
     }
-    
-    /** 
-     * 
+
+    /**
+     *
      * Register a tool as a tool class with a given harvest level.
      *
      * @param tool The custom tool to register.
      * @param toolClass The tool class to register as.  The predefined tool
-     *                  clases are "pickaxe", "shovel", "axe".  You can add 
+     *                  clases are "pickaxe", "shovel", "axe".  You can add
      *                  others for custom tools.
      * @param harvestLevel The harvest level of the tool.
      */
@@ -59,7 +64,7 @@ public class MinecraftForge
        ForgeHooks.toolClasses.put(tool, Arrays.asList(toolClass, harvestLevel));
    }
 
-   /** 
+   /**
     * Register a block to be harvested by a tool class.  This is the metadata
     * sensitive version, use it if your blocks are using metadata variants.
     * By default, this sets the block class as effective against that type.
@@ -80,13 +85,13 @@ public class MinecraftForge
        ForgeHooks.toolEffectiveness.add(key);
    }
 
-   /** 
+   /**
     * Remove a block effectiveness mapping.  Since setBlockHarvestLevel
     * makes the tool class effective against the block by default, this can be
     * used to remove that mapping.  This will force a block to be harvested at
     * the same speed regardless of tool quality, while still requiring a given
     * harvesting level.
-    * 
+    *
     * @param block The block to remove effectiveness from.
     * @param metadata The metadata for the block subtype.
     * @param toolClass The tool class to remove the effectiveness mapping from.
@@ -98,7 +103,7 @@ public class MinecraftForge
        ForgeHooks.toolEffectiveness.remove(key);
    }
 
-   /** 
+   /**
     * Register a block to be harvested by a tool class.
     * By default, this sets the block class as effective against that type.
     *
@@ -119,8 +124,8 @@ public class MinecraftForge
            ForgeHooks.toolEffectiveness.add(key);
        }
    }
-   
-   /** 
+
+   /**
     * Returns the block harvest level for a particular tool class.
     *
     * @param block The block to check.
@@ -141,13 +146,13 @@ public class MinecraftForge
        return harvestLevel;
    }
 
-   /** 
+   /**
     * Remove a block effectiveness mapping.  Since setBlockHarvestLevel
     * makes the tool class effective against the block by default, this can be
     * used to remove that mapping.  This will force a block to be harvested at
     * the same speed regardless of tool quality, while still requiring a given
     * harvesting level.
-    * 
+    *
     * @param block The block to remove effectiveness from.
     * @param toolClass The tool class to remove the effectiveness mapping from.
     * @see setToolClass for details on tool classes.
@@ -160,7 +165,7 @@ public class MinecraftForge
            ForgeHooks.toolEffectiveness.remove(key);
        }
    }
-   
+
    /**
     * Method invoked by FML before any other mods are loaded.
     */
@@ -186,9 +191,47 @@ public class MinecraftForge
        }
        EntityEnderman.carriableBlocks = temp;
    }
-   
+
    public static String getBrandingVersion()
    {
        return "Minecraft Forge "+ ForgeVersion.getVersion();
    }
+
+    /**
+     * Force a chunk to remain loaded by the supplied mod
+     *
+     * @param chunk the chunk to keep loaded
+     * @param world the world holding the chunk
+     * @param mod the mod (either {@link Mod} or {@link BaseMod}) that wishes to force the load
+     */
+    public static void forceChunkLoaded(ChunkCoordIntPair chunk, World world, Object mod)
+    {
+        ModContainer mc = FMLCommonHandler.instance().findContainerFor(mod);
+        if (mc == null)
+        {
+            FMLLog.warning("Attempt to force chunk load for a non-existent mod %s", mod.getClass().getName());
+            return;
+        }
+        String modId = mc.getModId();
+        world.getPersistentChunks().put(chunk, modId);
+    }
+
+    /**
+     * Stop forcing the chunk to remain loaded by the supplied mod (other mods may still
+     * keep the chunk loaded, however)
+     * @param chunk the chunk to keep loaded
+     * @param world the world holding the chunk
+     * @param mod the mod (either {@link Mod} or {@link BaseMod}) that wishes to unforce the load
+     */
+    public static void unforceChunkLoaded(ChunkCoordIntPair chunk, World world, Object mod)
+    {
+        ModContainer mc = FMLCommonHandler.instance().findContainerFor(mod);
+        if (mc == null)
+        {
+            FMLLog.warning("Attempt to unforce chunk load for a non-existent mod %s", mod.getClass().getName());
+            return;
+        }
+        String modId = mc.getModId();
+        world.getPersistentChunks().remove(chunk, modId);
+    }
 }
